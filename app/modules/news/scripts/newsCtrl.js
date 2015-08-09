b3b742a293c3ee2582c51bb55c2504aa2bf63c2e
Yuri Taratorkin <snicksnk@gmail.com>
2015-08-09 22:36:46 +0300

    [-] Removed urls config [m] newsCtrl

--- app/modules/news/scripts/newsCtrl.js
+++ app/modules/news/scripts/newsCtrl.js
@@ -1,6 +1,6 @@
 "use strict"
 angular.module('news')
-    .controller('NewsCtrl', function HomeCtrl($scope, $state, $modal, News) {
+    .controller('NewsCtrl', function($scope, $state, $modal, News) {
         News.getList().success(function(data) {
             $scope.data = data;
         });
